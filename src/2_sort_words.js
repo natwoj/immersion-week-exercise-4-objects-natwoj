7e9c3fdf10917e9853cd0409417f8c0b984d3cf1
natwoj <info@nwojcik.com>
2022-02-05 16:04:09 +0100

    return obj with even and odds

--- src/2_sort_words.js
+++ src/2_sort_words.js
@@ -1,3 +1,29 @@
-module.exports = function sortWords() {
-  // TODO Implement me.
-};
+module.exports = function sortWords (arr) {
+  let outputObj = 
+      {
+    even: "",
+    odd: ""
+      };
+  let tempArrEven = [];
+  let tempArrOdds = [];
+   
+  for (let i=0; i< arr.length; i++) {
+    // 1. arr with even
+  if ((arr[i].length % 2) == 0) {
+  tempArrEven.push(arr[i])
+   // 3. assign to key even
+  outputObj.even = tempArrEven;
+               }
+    // 2. arr with odds
+       else if {
+  tempArrOdds.push(arr[i])
+    // 3. assign to key odd
+  outputObj.odd = tempArrOdds;  
+   }
+    else if (arr.length === 0) {
+      // output 
+    }
+ }
+   return outputObj;
+ }
+ 
