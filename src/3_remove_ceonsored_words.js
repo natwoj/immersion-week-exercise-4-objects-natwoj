de8d72213e1eda957cfcfe13e547c0257c469fdb
natwoj <info@nwojcik.com>
2022-02-06 16:59:39 +0100

    finished

--- src/3_remove_ceonsored_words.js
+++ src/3_remove_ceonsored_words.js
@@ -1,3 +1,15 @@
 module.exports = function rmCensored() {
-  // TODO Implement me.
+  function rmCensored (obj) {
+    for (let [key, value] of Object.entries(obj)) {
+      if (key.includes("*") || value.includes("*")) {
+        delete obj[key], delete obj[value];
+        console.log(obj)
+      }
+    }
+    }
+    
+    const veggies = {"potato": "starch", "spin*ch": "green", "collard": "green", "tomato": "fr*it"}
+    rmCensored(veggies)
+    console.log(veggies) // { potato: 'starch', collard: 'green' } 
+  
 };
