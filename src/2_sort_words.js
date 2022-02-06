b474ed39551f7d7bed45c24fe6dcda20acf1f733
natwoj <info@nwojcik.com>
2022-02-06 14:30:48 +0100

    finished

--- src/2_sort_words.js
+++ src/2_sort_words.js
@@ -1,29 +1,22 @@
-module.exports = function sortWords (arr) {
+/*module.exports = */ 
+
+function sortWords (arr) {
   let outputObj = 
       {
-    even: "",
-    odd: ""
+    even: [],
+    odd: []
       };
-  let tempArrEven = [];
-  let tempArrOdds = [];
-   
   for (let i=0; i< arr.length; i++) {
-    // 1. arr with even
-  if ((arr[i].length % 2) == 0) {
-  tempArrEven.push(arr[i])
-   // 3. assign to key even
-  outputObj.even = tempArrEven;
-               }
-    // 2. arr with odds
-       else if {
-  tempArrOdds.push(arr[i])
-    // 3. assign to key odd
-  outputObj.odd = tempArrOdds;  
+    arr[i].length % 2 == 0 ? outputObj.even.push(arr[i]) : outputObj.odd.push(arr[i]);
    }
-    else if (arr.length === 0) {
-      // output 
-    }
- }
    return outputObj;
  }
+ sortWords(["work", "hard", "mommy"]);// {
+ // odd:  [ 'mommy' ],
+ // even: [ 'work', 'hard' ]
+ // }
  
+ sortWords(["oh!", "ah!", "hi", "my"]) // {
+ // odd:  [ 'oh!', 'ah!' ],
+ // even: [ 'hi', 'my' ] 
+ // })
