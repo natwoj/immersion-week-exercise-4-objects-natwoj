18728d8d3276a7e216117e2895785aa50e95c007
natwoj <info@nwojcik.com>
2022-02-12 15:43:14 +0100

    finished

--- src/5_group_families.js
+++ src/5_group_families.js
@@ -1,3 +1,26 @@
-module.exports = function groupFamilies() {
-  // TODO Implement me.
-};
+
+module.exports = groupFamilies;
+function groupFamilies(arr) {
+  let obj = {}; // definiuje pusty obiekt
+
+  for (let i = 0; i < arr.length; i++) { // przegladam wszytkie obiekty w tablicy
+    if (obj.hasOwnProperty(arr[i]['surname'])) {
+      // sprawdzam czy w pustym obiekcie znajduje się dane surname
+      obj[arr[i].surname].push(arr[i].name); // jesli tak to, przeniesc to tablicy z kluczem name
+    } else {
+      obj[arr[i].surname] = [arr[i].name]; //jezeli nie to stworz nowa pare klucz:value
+    }
+    return obj;
+  }
+}
+
+groupFamilies([
+  { name: 'Barack', surname: 'Obama' },
+  { name: 'Michelle', surname: 'Obama' },
+]); // { Obama: [ 'Barack', 'Michelle' ] }
+
+groupFamilies([
+  { name: 'Barack', surname: 'Obama' },
+  { name: 'Michelle', surname: 'Obama' },
+  { name: 'Donald', surname: 'Trump' },
+]); // { Obama: [ 'Barack', 'Michelle' ], Trump: [ 'Donald' ] }
