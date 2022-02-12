f51d0ccfa43ff608afaa9b78d7274936e6ec3fcb
natwoj <info@nwojcik.com>
2022-02-12 15:40:57 +0100

    finished

--- src/4_translate.js
+++ src/4_translate.js
@@ -1,3 +1,41 @@
 module.exports = function translate() {
-  // TODO Implement me.
+  
+function translate(obj, str) {
+  let newStr = str.split(' ');
+
+  for (let i = 0; i < newStr.length; i++ ){
+  if (obj.hasOwnProperty(newStr[i])) {    
+    newStr[i] = obj[newStr[i]]; 
+  } else {
+    console.log("Error: missing value");
+    return "Error: missing value";
+  }
+}
+
+console.log(newStr.join(" "));
+return newStr.join(" ");
+};
+translate({
+    "je": "I",
+    "suis": "am",
+    "pere": "father",    
+    "ton": "your"}, "je suis ton pere"
+) // 'I am your father'
+
+translate({
+    "the": "le",
+    "cute": "mignon",
+    "your": "ton",
+    "dog": "chien",
+    "is": "est"}, "the dog is cute"
+) // 'le chien est mignon'
+
+translate({
+    "the": "le",
+    "cute": "mignon",
+    "your": "ton",
+    "dog": "chien",
+    "is": "est"}, "the dog is fluffy"
+) // 'Error: missing value'module.exports = translate;
+
 };
