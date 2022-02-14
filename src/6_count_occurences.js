7f2d8cfe91b82c5f123682a40d8d0d61091bec99
natwoj <info@nwojcik.com>
2022-02-14 17:11:44 +0100

    step by step

--- src/6_count_occurences.js
+++ src/6_count_occurences.js
@@ -1,3 +1,36 @@
 module.exports = function countOccurences() {
-  // TODO Implement me.
+
+  // solution by Karolina 
+
+function countOccurences(str) {
+  let words = str.split(' '); //zamieniam zdanie na tablicę słów
+  let obj = {};
+  //w pętli for tworzę obiekt w którym zliczam poszczeólne słowa {słowo : ilość występowania}
+  for (let i=0; i < words.length; i++)
+  {
+    if (Object.keys(obj).includes(words[i])) {
+      obj[words[i]] += 1;
+    } else {
+      obj[words[i]] = 1;
+    }
+  }
+  let objRev = {};
+  //w pętli for tworzę obiekt w oparciu o stworzony wcześniej 'obj' zamieniam klucze z wartościami i zliczam:
+  //{ilość występowania : [słowa, które występują daną ilość razy]}
+  for (let key in obj) {
+    if (Object.keys(objRev).includes(obj[key].toString())){
+      objRev[obj[key]].push(key);
+    } else {
+      objRev[obj[key]] = [key];
+    }
+  }
+  return objRev;
 };
+
+countOccurences('hello is it you hello hello'); // {
+// '1': [ 'is', 'it', 'you' ],
+// '3': [ 'hello' ]
+// }
+countOccurences('hey mama hey mama'); // {
+// '2': [ 'hey', 'mama' ]
+// }
